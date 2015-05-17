c85d6cee04bbdb9b609215165a65b5b51e7ca204
ridwannaruto <rshariffdeen@gmail.com>
2015-05-17 14:10:12 +0530

    Admin Home fixed

--- src/Bundle/CoreBundle/Controller/HomeController.php
+++ src/Bundle/CoreBundle/Controller/HomeController.php
@@ -21,16 +21,31 @@ class HomeController extends BaseController {
             if ($userRole == $this->USER_ROLE_ADMIN) {
                 $memberList = $this->findAllEntities(RepositoryName::$REPOSITORY_USER);
                 $taskList = $this->getTaskListForAdmin();
+                $completedTaskInfo = count($this->findUniqueEntities(RepositoryName::$REPOSITORY_TASK,array('completed'=>1)));
+                $ongoingTaskInfo = count($this->findUniqueEntities(RepositoryName::$REPOSITORY_TASK,array('completed'=>0)));
+                $completedProjectInfo = count($this->findUniqueEntities(RepositoryName::$REPOSITORY_PROJECT,array('status'=>1)));
+                $ongoingProjectInfo = count($this->findUniqueEntities(RepositoryName::$REPOSITORY_PROJECT,array('status'=>0)));
+                $activeVolunteerInfo = count($this->findUniqueEntities(RepositoryName::$REPOSITORY_USER,array('status'=>2)));
+                $pendingVolunteerInfo = count($this->findUniqueEntities(RepositoryName::$REPOSITORY_USER,array('status'=>1)));
+
                 return $this->render(TwigTemplate::$TWIG_HOME_ADMIN, array(
                     $this->KEY_TASK_LIST => $taskList,
                     $this->KEY_NOTIFICATION_LIST => $notificationList,
-                    'members' => $memberList
+                    'members' => $memberList,
+                    'completedTaskInfo' => $completedTaskInfo,
+                    'ongoingTaskInfo' => $ongoingTaskInfo,
+                    'completedProjectInfo' => $completedProjectInfo,
+                    'ongoingProjectInfo' => $ongoingProjectInfo,
+                    'activeVolunteerInfo' => $activeVolunteerInfo,
+                    'pendingVolunteerInfo' =>$pendingVolunteerInfo
                 ));
             }
 
             if ($userRole == $this->USER_ROLE_HEAD) {
                 $searchParams = array('pillar'=>$authenticatedUser->getPillar());
                 $memberList = $this->findUniqueEntities(RepositoryName::$REPOSITORY_USER,$searchParams);
+
+
                 $taskList = $this->getTaskListForHead($authenticatedUser->getPillar());
                 return $this->render(TwigTemplate::$TWIG_HOME_HEAD, array(
                     $this->KEY_TASK_LIST => $taskList,
