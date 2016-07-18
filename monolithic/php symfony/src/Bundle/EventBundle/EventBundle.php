ef5c34c2a3afe94cdec7344491fc0096a394e84a
rshariffdeen <rshariffdeen@gmail.com>
2016-07-18 11:29:57 +0530

    remove php code

--- monolithic/php symfony/src/Bundle/EventBundle/EventBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Bundle\EventBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class EventBundle extends Bundle
-{
-}
--- monolithic/php symfony/src/Bundle/TaskBundle/TaskBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Bundle\TaskBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class TaskBundle extends Bundle
-{
-}
