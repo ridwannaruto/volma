0d67ae84870e4a4bbece9d47fcff22e90329aed5
rshariffdeen <rshariffdeen@gmail.com>
2016-07-18 12:25:38 +0530

    adding DockerFile

--- server.js
+++ server.js
@@ -4,19 +4,18 @@
  *
  */
 
+'use strict';
 
-var http = require("http");
+const express = require('express');
 
-http.createServer(function (request, response) {
+// Constants
+const PORT = 8081;
 
-    // Send the HTTP header
-    // HTTP Status: 200 : OK
-    // Content Type: text/plain
-    response.writeHead(200, {'Content-Type': 'text/plain'});
+// App
+const app = express();
+app.get('/', function (req, res) {
+    res.send('Hello world\n');
+});
 
-    // Send the response body as "Hello World"
-    response.end('Hello World\n');
-}).listen(8081);
-
-// Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
+app.listen(PORT);
+console.log('Running on http://localhost:' + PORT);
